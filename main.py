3ed42c125a10a5a1d4247d3e393d041d128f570f
Odiagbe Samson Osaro <sam.odiagbe.dev@gmail.com>
2025-07-30 17:06:30 -0400

    cleaning up the data

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ crime_data = pd.read_csv("./data/toronto_crime_data.csv")
 print(crime_data.columns)
 crime_data.drop(columns=["OBJECTID", "EVENT_UNIQUE_ID", "x", "y", "OCC_DAY", "OCC_YEAR", "OCC_MONTH"], inplace=True)
 
-FEATURES = ["REPORT_DATE", "OCC_DATE", "REPORT_YEAR", "REPORT_MONTH", "REPORT_DOW", "REPORT_HOUR", "OCC_YEAR", "OCC_MONTH", "OCC_DAY", "OCC_HOUR", "DIVISION", "LOCATION_TYPE", "PREMISES_TYPE", "UCR_CODE", "OFFENCE", "MCI_CATEGORY", "NEIGHBOURHOOD_158", "NEIGHBOURHOOD_140", "LONG_WGS84", "LAT_WGS84"]
+FEATURES = ["REPORT_DATE", "OCC_DATE", "REPORT_YEAR", "REPORT_MONTH", "REPORT_DOW", "REPORT_HOUR", "OCC_HOUR", "DIVISION", "LOCATION_TYPE", "PREMISES_TYPE", "UCR_CODE", "OFFENCE", "MCI_CATEGORY", "NEIGHBOURHOOD_158", "NEIGHBOURHOOD_140", "LONG_WGS84", "LAT_WGS84"]
 
 crime_data = crime_data[FEATURES]
 
-print(null_entries)
+
 print(crime_data.isnull().sum())
 
 
