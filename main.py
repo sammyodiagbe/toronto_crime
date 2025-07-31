fdec1f61e7d00af491b2dfbfff4566a33e787e58
Odiagbe Samson Osaro <sam.odiagbe.dev@gmail.com>
2025-07-31 11:21:39 -0400

    made map load more efficiently

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ crime_data.drop(columns=["OBJECTID", "EVENT_UNIQUE_ID", "x", "y", "OCC_DAY", "OC
 
 FEATURES = ["REPORT_DATE", "OCC_DATE", "REPORT_YEAR", "REPORT_MONTH", "REPORT_DOW", "REPORT_HOUR", "OCC_HOUR", "DIVISION", "LOCATION_TYPE", "PREMISES_TYPE", "UCR_CODE", "OFFENCE", "MCI_CATEGORY", "NEIGHBOURHOOD_158", "NEIGHBOURHOOD_140", "LONG_WGS84", "LAT_WGS84"]
 
-crime_data = crime_data[0:1000][FEATURES]
+crime_data = crime_data[FEATURES]
 
 
 crime_data["OCC_DATE"] = pd.to_datetime(crime_data["OCC_DATE"])
