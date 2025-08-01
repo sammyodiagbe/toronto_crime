394334f0d479a025703bd376f94eec9eb5b89a7f
Odiagbe Samson Osaro <sam.odiagbe.dev@gmail.com>
2025-07-31 21:53:22 -0400

    cleaning up the data and also encoding the label

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from folium.plugins import MarkerCluster
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error, classification_report
+from sklearn.preprocessing import LabelEncoder
+
+encoder = LabelEncoder()
 
 crime_data = pd.read_csv("./data/toronto_crime_data.csv")
 map_cord = [43.651070, -79.347015]
@@ -21,12 +24,22 @@ color_map = {
     "Auto Theft": "darkred",
     # Add more if needed
 }
+crime_data["OFFENCE"] = encoder.fit_transform(crime_data["OFFENCE"])
+crime_data["NEIGHBOURHOOD_158"] = encoder.fit_transform(crime_data["NEIGHBOURHOOD_158"])
+crime_data["NEIGHBOURHOOD_140"] = encoder.fit_transform(crime_data["NEIGHBOURHOOD_140"])
+
+
+
+# crime_data.drop(["OFFENCE", "NEIGHBOURHOOD_158", "NEIGHBOURHOOD_140"], inplace=True)
+
 
 marker_cluster = MarkerCluster().add_to(crime_map)
 
-crime_data.drop(columns=["OBJECTID", "EVENT_UNIQUE_ID", "x", "y", "OCC_DAY", "OCC_YEAR", "OCC_MONTH"], inplace=True)
 
-FEATURES = ["REPORT_DATE", "OCC_DATE", "REPORT_YEAR", "REPORT_MONTH", "REPORT_DOW", "REPORT_HOUR", "OCC_HOUR", "DIVISION", "LOCATION_TYPE", "PREMISES_TYPE", "UCR_CODE", "OFFENCE", "MCI_CATEGORY", "NEIGHBOURHOOD_158", "NEIGHBOURHOOD_140", "LONG_WGS84", "LAT_WGS84"]
+
+crime_data.drop(columns=["OBJECTID", "EVENT_UNIQUE_ID", "x", "y", "OCC_DAY", "OCC_YEAR", "OCC_MONTH", "OCC_DOW", "REPORT_DOW"], inplace=True)
+
+FEATURES = ["REPORT_DATE", "OCC_DATE", "REPORT_YEAR", "REPORT_MONTH", "REPORT_HOUR", "OCC_HOUR", "DIVISION", "LOCATION_TYPE", "PREMISES_TYPE", "UCR_CODE", "OFFENCE", "MCI_CATEGORY", "NEIGHBOURHOOD_158", "NEIGHBOURHOOD_140", "LONG_WGS84", "LAT_WGS84"]
 
 crime_data = crime_data[FEATURES]
 
@@ -36,20 +49,23 @@ crime_data["OCC_YEAR"] = crime_data["OCC_DATE"].dt.year
 crime_data["OCC_MONTH"] = crime_data["OCC_DATE"].dt.month
 crime_data["OCC_DAY"] = crime_data["OCC_DATE"].dt.day
 crime_data["OCC_HOUR"] = crime_data["OCC_DATE"].dt.hour
-crime_data["OCC_DOW"] = crime_data["OCC_DATE"].dt.day_of_week
 
-for long, lat, crime_type  in zip(crime_data['LONG_WGS84'],  crime_data['LAT_WGS84'], crime_data['MCI_CATEGORY']):
-    folium.CircleMarker(
-            location=[lat, long],
-            radius=2,
-            popup=f"{crime_type}",
-            color=color_map.get(crime_type, "gray"),
-            fill=True
-    ).add_to(crime_map)
+crime_data.drop(columns=["OCC_DATE", "REPORT_DATE", "REPORT_MONTH"], inplace=True)
+
+# for long, lat, crime_type  in zip(crime_data['LONG_WGS84'],  crime_data['LAT_WGS84'], crime_data['MCI_CATEGORY']):
+#     folium.CircleMarker(
+#             location=[lat, long],
+#             radius=2,
+#             popup=f"{crime_type}",
+#             color=color_map.get(crime_type, "gray"),
+#             fill=True
+#     ).add_to(crime_map)
 
 
 y_pred = crime_data["MCI_CATEGORY"]
 x = crime_data.drop(columns=["MCI_CATEGORY"])
+
+
 
 train_x, test_x, train_y, test_y = train_test_split(x, y_pred, test_size=0.2, random_state=42)
 
